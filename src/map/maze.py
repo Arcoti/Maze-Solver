732d1d94dcac614e249d9190ab804ebabab450ad
SY <ArchdukeArcoti@gmail.com>
2025-06-04 13:01:21 +0800

    Added a simple map

--- src/map/maze.py
+++ src/map/maze.py
@@ -55,3 +55,21 @@ class Maze:
     
     def validateExit(self):
         return any(square.role == Role.EXIT for square in self), "There must be at least one exit."
+    
+if __name__ == "__main__":
+    maze = Maze(
+        squares=(
+            Square(0, (0, 0), Border.NORTH | Border.EAST),
+            Square(1, (0, 1), Border.NORTH | Border.WEST),
+            Square(2, (0, 2), Border.EAST | Border.WEST, Role.EXIT),
+            Square(3, (0, 3), Border.NORTH | Border.EAST | Border.WEST),
+            Square(4, (1, 0), Border.SOUTH | Border.EAST | Border.WEST),
+            Square(5, (1, 1), Border.EAST | Border.WEST),
+            Square(6, (1, 2), Border.SOUTH | Border.EAST),
+            Square(7, (1, 3), Border.WEST),
+            Square(8, (2, 0), Border.NORTH | Border.EAST, Role.ENTRANCE),
+            Square(9, (2, 1), Border.SOUTH),
+            Square(10, (2, 2), Border.NORTH | Border.SOUTH),
+            Square(11, (2, 3), Border.SOUTH | Border.WEST),
+        )
+    )
