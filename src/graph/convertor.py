cacf3a4d3c52bace8f605fe9f6a58968fdbe789b
SY <ArchdukeArcoti@gmail.com>
2025-06-04 15:10:42 +0800

    Fix bug in retrieving edges

--- src/graph/convertor.py
+++ src/graph/convertor.py
@@ -4,6 +4,8 @@ from ..model.role import Role
 from .edge import Node
 from .edge import Edge
 
+from ..model.square import Square # For main function, delete when not needed
+
 class Convertor:
     nodes: set[Node] = set()
     edges: set[Edge] = set()
@@ -38,11 +40,13 @@ class Convertor:
                     cls.edges.add(Edge(sourceNode, node))
                     break
             
+            node = sourceNode
+            
             # Move down
             for y in range(node.coordinate[0] + 1, maze.height):
                 if node.border & Border.SOUTH:
                     break
-
+                    
                 node = maze[y * maze.width + node.coordinate[1]]
 
                 if node in cls.nodes:
@@ -54,3 +58,28 @@ class Convertor:
     @classmethod
     def convertToGraph(cls, maze: Maze):
         return (cls.getNodes(maze), cls.getEdges(maze))
+    
+if __name__ == "__main__":
+    maze = Maze(
+        squares=(
+            Square(0, (0, 0), Border.NORTH | Border.WEST),
+            Square(1, (0, 1), Border.NORTH | Border.EAST),
+            Square(2, (0, 2), Border.WEST | Border.EAST, Role.EXIT),
+            Square(3, (0, 3), Border.NORTH | Border.WEST | Border.EAST),
+            Square(4, (1, 0), Border.SOUTH | Border.WEST | Border.EAST),
+            Square(5, (1, 1), Border.WEST | Border.EAST),
+            Square(6, (1, 2), Border.SOUTH | Border.WEST),
+            Square(7, (1, 3), Border.EAST),
+            Square(8, (2, 0), Border.NORTH | Border.WEST, Role.ENTRANCE),
+            Square(9, (2, 1), Border.SOUTH),
+            Square(10, (2, 2), Border.NORTH | Border.SOUTH),
+            Square(11, (2, 3), Border.SOUTH | Border.EAST),
+        )
+    )
+
+    nodes, edges = Convertor.convertToGraph(maze)
+    for node in nodes:
+        print(node.coordinate)
+    
+    for edge in edges:
+        print(edge.source.coordinate, " -> ", edge.destination.coordinate)
