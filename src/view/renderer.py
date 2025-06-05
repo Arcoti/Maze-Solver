bfbfbe003cf6c23360caf36c61fd3c64358dcde7
SY <ArchdukeArcoti@gmail.com>
2025-06-05 13:24:22 +0800

    Fix minor typo in renderer.py

--- src/view/renderer.py
+++ src/view/renderer.py
@@ -117,7 +117,7 @@ class SVGRenderer:
             ]
         ).draw(
             stroke_width = self.lineWidth // 2,
-            strok_opacity = "50%",
+            stroke_opacity = "50%",
             stroke = "red",
             fill = "none",
             marker_end = "url(#arrow)",
