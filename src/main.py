e9daa11c15fb5b9533a7576796a4e2c4ab6f041d
SY <ArchdukeArcoti@gmail.com>
2025-06-12 10:19:56 +0800

    Finalize code and updated Readme

--- src/main.py
+++ src/main.py
@@ -10,27 +10,10 @@ from .view.renderer import SVGRenderer
 from .persistence.serializer import dump, load
 
 if __name__ == "__main__":
-    maze = Maze(
-        squares=(
-            Square(0, (0, 0), Border.NORTH | Border.WEST),
-            Square(1, (0, 1), Border.NORTH | Border.EAST),
-            Square(2, (0, 2), Border.WEST | Border.EAST, Role.EXIT),
-            Square(3, (0, 3), Border.NORTH | Border.WEST | Border.EAST), 
-            Square(4, (1, 0), Border.SOUTH | Border.WEST | Border.EAST),
-            Square(5, (1, 1), Border.WEST | Border.EAST),
-            Square(6, (1, 2), Border.SOUTH | Border.WEST),
-            Square(7, (1, 3), Border.EAST),
-            Square(8, (2, 0), Border.NORTH | Border.WEST, Role.ENTRANCE),
-            Square(9, (2, 1), Border.SOUTH),
-            Square(10, (2, 2), Border.NORTH | Border.SOUTH),
-            Square(11, (2, 3), Border.SOUTH | Border.EAST),
-        )   
-    )
-    
     currentDirectory = os.getcwd()
     filePath = currentDirectory + "\\static\\miniature.maze"
 
-    dump(maze, Path(filePath))
+    # Load the sample miniature maze
     maze = load(Path(filePath))
 
     # Solve the maze
@@ -38,12 +21,11 @@ if __name__ == "__main__":
 
     # Render the maze solution
     renderer = SVGRenderer()
-    svg = renderer.render(maze, solution)
+    # svg = renderer.render(maze, solution)
 
-    # Save the maze solution as an svg file
-    with Path("static/maze.svg").open(mode="w", encoding="utf-8") as file:
-        file.write(svg.xmlContent)
+    # # Save the maze solution as an svg file
+    # with Path("static/maze.svg").open(mode="w", encoding="utf-8") as file:
+    #     file.write(svg.xmlContent)
 
-    # Offer previw
-    renderer.render(maze).preview()
+    # Preview of Solution
     renderer.render(maze, solution).preview()
